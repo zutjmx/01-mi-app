f1f1e0478a08b5db6c1873d4935a558d1849c3c5
Jesus Zuñiga <jesuszt@hotmail.com>
2021-10-28 13:40:26 -0500

    Cambios 28/10/2021

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit() {
+
+    let numero = 10;
+    //debugger;
+
+    numero = 20;
+
+    numero = 30;
+
+    numero = 0;
+
+    console.log('Hola Mundo con Ionic');
+  }
 
 }
